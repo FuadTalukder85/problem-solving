3452ef52a480029950d8fd1279cfac1b6c51ccd5
Fuad Talukder <webfuadtk@gmail.com>
2024-05-23 17:00:09 +0600

    solve problen one

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const person = [
   { name: "Fuad", age: 25, gender: "male" },
-  { name: "Talukder", age: 23, gender: "male" },
   { name: "Sokina", age: 18, gender: "female" },
+  { name: "Taluder", age: 20, gender: "male" },
+  { name: "Jorina", age: 19, gender: "female" },
 ];
+
+const AllFemales = (person) => {
+  const males = person.filter((male) => male.gender === "male");
+  const male = males.map((male) => male.name);
+  return male;
+};
+console.log(AllFemales(person));
