2c4a8f54eadfc5d8f29938c10ac23f322d23592b
Fuad Talukder <webfuadtk@gmail.com>
2024-05-23 17:43:50 +0600

    solve second problem

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// 1. Task: Array Filtering And Mapping
+// Create An Array Of Objects, Each Representing A Person With Properties Like Name, Age, And Gender. Write A Function To Filter Out All Females And Then Map The Remaining People To An Array Of Names. Print The Final Result.
+
 const person = [
   { name: "Fuad", age: 25, gender: "male" },
   { name: "Sokina", age: 18, gender: "female" },
@@ -11,3 +14,18 @@ const AllFemales = (person) => {
   return male;
 };
 console.log(AllFemales(person));
+
+// 2. Task: Object Manipulation
+// Create An Array Of Objects Representing Books With Properties Like Title, Author, And Year. Write A Function That Takes The Array And Returns A New Array With Only The Book Titles. Print The Result.
+
+const books = [
+  { title: "amar boi", author: "Fuad", year: "5 may 2024" },
+  { title: "tomar boi", author: "Talukder", year: "4 may 2023" },
+];
+
+const BookTitle = (books) => {
+  const title = books.map((bookTile) => bookTile.title);
+  return title;
+};
+
+console.log(BookTitle(books));
